03d8830555126e34e8d8632d9b34332bea310354
mystar1995 <jws19950122@outlook.com>
2020-09-13 11:39:21 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,43 @@
 <?php
+Route::get('/', function() {
+    return redirect(route('admin.dashboard'));
+});
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('home', function() {
+    return redirect(route('admin.dashboard'));
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::name('admin.')->prefix('admin')->middleware('auth')->group(function() {
+    Route::get('dashboard', 'DashboardController')->name('dashboard');
+    Route::get('speak_up', 'SpeakController')->name('speak_up');
+    Route::get('test', 'TestController')->name('test');
+    Route::get('courses', 'CoursesController')->name('courses');
+    Route::get('note_side', 'NotesideController')->name('note_side');
+    Route::get('request', 'RequestController')->name('request');
+    Route::get('notification', 'NotificationController')->name('notification');
+    Route::get('analytics', 'AnalyticsController')->name('analytics');
+    Route::get('setting', 'SettingController')->name('setting');
+
+    Route::get('users/roles', 'UserController@roles')->name('users.roles');
+    Route::resource('users', 'UserController', [
+        'names' => [
+            'index' => 'users'
+        ]
+    ]);
+});
+
+Route::middleware('auth')->get('logout', function() {
+    Auth::logout();
+    return redirect(route('login'))->withInfo('You have successfully logged out!');
+})->name('logout');
+
+Auth::routes(['verify' => true]);
+
+Route::name('js.')->group(function() {
+    Route::get('dynamic.js', 'JsController@dynamic')->name('dynamic');
+});
+
+// Get authenticated user
+Route::get('users/auth', function() {
+    return response()->json(['user' => Auth::check() ? Auth::user() : false]);
 });
