bc6c4c8607456880f24299be0d820667de2ba37a
mystar1995 <jws19950122@outlook.com>
2020-09-13 14:30:41 +0800

    init

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(User::class);
+        //$this->authorizeResource(User::class);
     }
     /**
      * Display a listing of the resource.
