5ecb20735aee09151c5f8656f8e75ca7a572f32b
mystar1995 <jws19950122@outlook,com>
2020-10-16 15:41:58 +0800

    saerfd

--- resources/views/layouts/admin-master.blade.php
+++ resources/views/layouts/admin-master.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>@yield('title', 'Stisla Laravel') &mdash; {{ env('APP_NAME') }}</title>
+  <title>@yield('title', 'Stisla Laravel') &mdash; WatchMyCrew</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- General CSS Files -->
   <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
